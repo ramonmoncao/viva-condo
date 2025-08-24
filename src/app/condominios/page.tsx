aaaa0b1a06d9bd9ff5eee17c31cf8e52baf5c7b7
Ramon Moncao <ramonmoncao@hotmail.com>
2025-08-23 22:37:45 -0300

    adição do service

--- src/app/condominios/page.tsx
+++ src/app/condominios/page.tsx
@@ -1,7 +1,24 @@
 "use client";
 
+import { useEffect, useState } from 'react'
+import { getCondominios, ICondominio } from '@/services/api-condominios';
+
 export default function ListaCondominios(){
-    return(
+
+    const [condominios, setCondominios] = useState<ICondominio[]>([])
+
+    useEffect(()=>
+    {
+        const buscarCondominios = async () => {
+            const data = await getCondominios()
+            console.log("data" + data)
+            setCondominios(data)
+        }
+
+        buscarCondominios()
+    }, [])
+
+        return(
         <div className="p-6 max-w full">
             <div className="mb-4 flex items-center justify-between gap-4">
             <h1 className="text-t1 font-semibold">Condomínios</h1>
@@ -11,21 +28,33 @@ export default function ListaCondominios(){
             <table className="min-w-full divide-y divide-gray-200">
             <thead className="bg-gray-50">
             <tr>
-                <th className="px-4 py-3 text-left text-xs font-medium text-grey-500 tracking-wider w-12">#</th>
-                <th className="px-4 py-3 text-left text-xs font-medium text-grey-500 w-12">Nome</th>
-                <th className="px-4 py-3 text-left text-xs font-medium text-grey-500 w-12">Endereço</th>
-                <th className="px-4 py-3 text-left text-xs font-medium text-grey-500 w-12">Cidade</th> 
-                <th className="px-4 py-3 text-left text-xs font-medium text-grey-500 w-12">UF</th>
-                <th className="px-4 py-3 text-left text-xs font-medium text-grey-500 w-12">Tipo</th>
-                <th className="px-4 py-3 text-left text-xs font-medium text-grey-500 w-12">Ação</th>       
+                <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider w-12">#</th>
+                <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">Nome</th>
+                <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">Endereço</th>
+                <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">Cidade</th> 
+                <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">UF</th>
+                <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">Tipo</th>
+                <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">Ação</th>       
             </tr>
             </thead>
             <tbody className="divide-y divide-gray-200 bg-white">
+                {condominios.length === 0 ? (
             <tr>
                 <td className="px-4 py-3 text-sm text-gray-700" colSpan={7}>
                     Nenhum condominio encontrado.
                 </td>
-            </tr>    
+            </tr>) : ( condominios.map((condominio, index) => (
+                <tr key={condominio.id_condominio} className='hover>bg-gray-50'>
+                <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{String(index + 1)}</td>
+                <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{condominio.nome_condominio}</td>
+                <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{condominio.endereco_condominio}</td>
+                <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{condominio.cidade_condominio}</td>
+                <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{condominio.uf_condominio}</td>
+                <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{condominio.tipo_condominio}</td>
+                <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500"></td>
+                </tr>
+            ))
+            )}
             </tbody> 
         </table>           
         </div>
